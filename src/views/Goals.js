91420efb842a1c2d33f4abef3105358a47162564
Dimitrius Mckinnon <dimitriusmckinnon419@gmail.com>
2023-11-30 02:37:27 -0500

    final commit of all files. Goals page is built out and accepts fetch request for custom mock api consisting of nutritional meals. Then displays meals in appropriately styled list. Settings page built out with simple input forms and content to make use of page.

--- src/views/Goals.js
+++ src/views/Goals.js
@@ -1,11 +1,36 @@
-
+import React, { useState, useEffect } from "react";
 
 function Goals() {
-  return (
-    <div className="container">
-      <h1>Goals Page</h1>
-    </div>
-  );
+
+    const [fitnessData, setFitnessData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://6567b2d09927836bd973c1d2.mockapi.io/my/api/fitness')
+            .then((response) => response.json())
+            .then((fitnessData) => setFitnessData(fitnessData))
+        console.log(fitnessData);
+    }, []);
+    return (
+        <div>
+            <h1 className="d-flex align-items-center flex-column greeting-container">Nutritional Meals To Meet Your Goals</h1>
+            <ul>
+                {fitnessData.map(item => (
+                    <i><h2 className="bi bi-dash-circle" key={item.id}> {item.name} </h2>
+                        <ul>
+                            {item.ingredients.map(ingredient => (
+                                <li key={ingredient.name}>
+                                    <i><h3 className="text-muted bi bi-arrow-90deg-up">{ingredient.name}: {ingredient.measurement}</h3> - Carbs: {ingredient.carbs}g, Fats: {ingredient.fats}g, Proteins: {ingredient.protein}g</i>
+                                    
+                                </li>
+                                
+                            ))}
+                            <br />
+                        </ul>
+                    </i>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Goals;
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -4,8 +4,38 @@ function Settings() {
     return (
         <div className="container">
             <h1>Settings Page</h1>
+
+            <section>
+                <h2>Account Settings</h2>
+                <p>Change your password:</p>
+                <button>Change Password</button>
+                <p>Manage your email preferences:</p>
+                <button>Email Preferences</button>
+                <p>Set up two-factor authentication:</p>
+                <button>Two-Factor Authentication</button>
+            </section>
+
+            <section>
+                <h2>Profile Settings</h2>
+                <p>Upload a profile picture or avatar:</p>
+                <input type="file" />
+                <p>Set your display name:</p>
+                <label>Display Name: <input type="text" /></label>
+                <p>Write a short bio:</p>
+                <textarea placeholder="Write a short bio"></textarea>
+            </section>
+
+            <section>
+                <h2>Notification Settings</h2>
+                <p>Manage your notification preferences:</p>
+                <button>Manage Notifications</button>
+                <p>Receive promotional emails:</p>
+                <label>
+                    <input type="checkbox" /> Yes, I want to receive promotional emails
+                </label>
+            </section>
         </div>
-    )
+    );
 }
 
 export default Settings;
